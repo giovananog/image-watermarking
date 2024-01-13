813b3d02145e7d3d94532c9dece808fdd4ffdb10
Giovana Nogueira <ongiovana2016@gmail.com>
2024-01-13 15:35:52 -0300

    feat(ui): update UI design with tkinter

---
+++ ui.py
@@ -0,0 +1,49 @@
+from tkinter import *
+
+# constants to color and fonts
+BLACK = "#1C1C1C"
+GRAY = "#DCDCDC"
+FONT_NAME = "Courier"
+
+# start a new TK window to the application
+window = Tk()
+window.title("Image Watermarking")
+window.config(padx=20, pady=16, bg=GRAY)
+
+title_label = Label(text="Watermarking App", fg=BLACK, bg=GRAY, font=(FONT_NAME, 30))
+title_label.grid(column=1, row=0)
+
+# new canvas to manipulate the images
+canvas = Canvas(width=320, height=224, bg=BLACK, highlightthickness=0)
+canvas.grid(column=1, row=1)
+
+# this frame contains all options that should be choosen by the user before watermarking the image
+frame = Frame(window, padx=10, pady=10, bd=1, relief='solid') 
+frame.grid(column=0, row=1) 
+
+text_entry = Entry(frame)
+text_entry.insert(0, 'WATERMARK TEXT')
+text_entry.grid(column=0, row=2)
+
+upload_button = Button(frame, text="Upload Image", highlightthickness=0)
+upload_button.grid(column=0, row=3, pady=6)
+
+# button and label to choose and show color
+selext_color_button = Button(frame, text="Pick a color")
+selext_color_button.grid(column=1, row=1, pady=6)
+
+label_color = Label(frame, text=f"{GRAY}", width=10, height=2, relief="solid", bg=GRAY)
+label_color.grid(column=0, row=1, pady=6)
+
+
+# button out of frame 
+watermark_button = Button(text="Watermark Image", highlightthickness=0)
+watermark_button.grid(column=0, row=2)
+
+
+# save button 
+save_button = Button(text="Save Image", highlightthickness=0)
+save_button.grid(column=2, row=4)
+
+# keeps the app running all the time until it gets manually closed
+window.mainloop()
